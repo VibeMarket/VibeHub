e0de6d209bcd03cd43e3e767c2eef5752906b34b
Kevin D <kevin.diangelis@gmail.com>
2022-08-17 17:52:16 -0400

    add stuff

--- src/me/kevind/utils/ColorUtils.java
+++
@@ -1,9 +0,0 @@
-package me.kevind.utils;
-
-import org.bukkit.ChatColor;
-
-public class ColorUtils {
-    public static String color(String string) {
-        return ChatColor.translateAlternateColorCodes('&', string);
-    }
-}
