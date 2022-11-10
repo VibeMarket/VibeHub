79bc22c1b66d0b1ea898f0b205238bf23ae4619c
Kevin D <kevin.diangelis@gmail.com>
2022-11-09 22:47:30 -0500

    remove glass from selector because it has unforeseen issues

--- src/me/kevind/inventory/SelectorGUI.java
+++ src/me/kevind/inventory/SelectorGUI.java
@@ -15,15 +15,6 @@ public class SelectorGUI {
 
     public SelectorGUI() {
         inv = Bukkit.createInventory(null, 27, "Server Selector");
-        for (int i = 0; i < 9; i++) {
-            inv.setItem(i, ItemList.PLACEHOLDER_PURPLE_GLASS);
-        }
-        for (int i = 9; i < 18; i++) {
-            inv.setItem(i, ItemList.PLACEHOLDER_GLASS);
-        }
-        for (int i = 18; i < 27; i++) {
-            inv.setItem(i, ItemList.PLACEHOLDER_PURPLE_GLASS);
-        }
         inv.setItem(12, ItemList.SURVIVAL_SERVER);
         inv.setItem(13, ItemList.SKYBLOCK_SERVER);
         inv.setItem(14, ItemList.CREATIVE_SERVER);
