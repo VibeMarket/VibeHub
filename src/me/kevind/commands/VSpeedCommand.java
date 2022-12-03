087706497f67bb320d8eaeebd558406fc3589ab1
Kevin D <kevin.diangelis@gmail.com>
2022-12-03 15:35:53 -0500

    Have vspeed open the selector inventory instead of only toggling speed

--- src/me/kevind/commands/VSpeedCommand.java
+++ src/me/kevind/commands/VSpeedCommand.java
@@ -13,22 +13,12 @@ public class VSpeedCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         Player player = (Player) sender;
-        String vspeedinvalidargs = VibeHub.getInstance().getConfig().getString("messages.VSpeedInvalidArgs");
         String NoPermissionMessage = VibeHub.getInstance().getConfig().getString("messages.NoPermissionMessage");
         if (sender instanceof Player) {
-            if (args[0].equalsIgnoreCase("on")) {
-                String IWantSpeedMessage = VibeHub.getInstance().getConfig().getString("messages.IWantSpeedMessage");
-                player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 2));
-                player.sendMessage(ColorUtils.color(VibeHub.getPrefix() + IWantSpeedMessage));
-            }
-            if (args[0].equalsIgnoreCase("off")) {
-                String IDontWantSpeedMessage = VibeHub.getInstance().getConfig().getString("messages.IDontWantSpeedMessage");
-                for (PotionEffect effect : player.getActivePotionEffects())
-                    player.removePotionEffect(effect.getType());
-                player.sendMessage(ColorUtils.color(VibeHub.getPrefix() + IDontWantSpeedMessage));
-            }
-            if (args[0] == null) {
-                player.sendMessage(ColorUtils.color(VibeHub.getPrefix() + vspeedinvalidargs));
+            if (player.hasPermission("vibe.staff")) {
+                player.openInventory(VibeHub.getStaffselector().getInv());
+            }else {
+                player.openInventory(VibeHub.getSelectorGUI().getInv());
             }
         } else {
             player.sendMessage(NoPermissionMessage);
