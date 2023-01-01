77ce9b1677a1eab7679caa7585fd5e78e7040ded
Kevin D <kevin.diangelis@gmail.com>
2023-01-01 15:56:57 -0500

    make speed selector sugar

--- src/me/kevind/utils/ItemList.java
+++ src/me/kevind/utils/ItemList.java
@@ -14,10 +14,10 @@ public final class ItemList {
     public static final ItemStack SURVIVAL_SERVER = new ItemMaker(Material.GRASS_BLOCK).name("&a&lSurvival").lore("&7Version: &9" + survivalversion, "&7Teleport to the survival server").build();
     public static final ItemStack CREATIVE_SERVER = new ItemMaker(Material.BIRCH_WOOD).name("&6&lCreative").lore("&7Version: &9" + creativeversion, "&7Teleport to the creative server").build();
     public static final ItemStack SKYBLOCK_SERVER = new ItemMaker(Material.OAK_WOOD).name("&b&lSkyblock").lore("&7Version: &9" + skyblockversion, "&7Teleport to the skyblock server").build();
-    public static final ItemStack SPEED_ITEM = new ItemMaker(Material.BLAZE_ROD).name("&c&lSpeed Selector &7(Right Click)").lore("&7Set your speed!").build();
-    public static final ItemStack SPEED_3 = new ItemMaker(Material.BLAZE_ROD).name("&c&lSpeed 3").lore("&7Give yourself Speed 3").build();
-    public static final ItemStack SPEED_6 = new ItemMaker(Material.BLAZE_ROD).name("&c&lSpeed 6").lore("&7Give yourself Speed 6").build();
-    public static final ItemStack SPEED_9 = new ItemMaker(Material.BLAZE_ROD).name("&c&lSpeed 9").lore("&7Give yourself Speed 9").build();
+    public static final ItemStack SPEED_ITEM = new ItemMaker(Material.SUGAR).name("&c&lSpeed Selector &7(Right Click)").lore("&7Set your speed!").build();
+    public static final ItemStack SPEED_3 = new ItemMaker(Material.SUGAR).name("&c&lSpeed 3").lore("&7Give yourself Speed 3").build();
+    public static final ItemStack SPEED_6 = new ItemMaker(Material.SUGAR).name("&c&lSpeed 6").lore("&7Give yourself Speed 6").build();
+    public static final ItemStack SPEED_9 = new ItemMaker(Material.SUGAR).name("&c&lSpeed 9").lore("&7Give yourself Speed 9").build();
     public static final ItemStack NO_SPEED = new ItemMaker(Material.BARRIER).name("&c&lDisable Speed").lore("&7Disable speed").build();
     public static final ItemStack ENDERPEARL = new ItemMaker(Material.ENDER_PEARL).name("&d&lEnderpearl &7(Right Click)").lore("&7Throw me to ride!").build();
     public static final ItemStack BUILD_SERVER = new ItemMaker(Material.COMMAND_BLOCK).name("&c&lBuild").lore("&7Version: &9" + staffversion, "&7Teleport to the Build server.", "&7You are staff so you can see this server.").build();
