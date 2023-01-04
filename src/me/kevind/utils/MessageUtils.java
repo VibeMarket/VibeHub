ca0fd19940c078a97b43e04ec3e1f391668d507a
Kevin D <kevin.diangelis@gmail.com>
2023-01-04 15:30:21 -0500

    new broadcastMessage method in messageutils
    Signed-off-by: Kevin D <kevin.diangelis@gmail.com>

--- src/me/kevind/utils/MessageUtils.java
+++ src/me/kevind/utils/MessageUtils.java
@@ -1,10 +1,14 @@
 package me.kevind.utils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class MessageUtils {
     public static void sendMessage(CommandSender sender, String s) {
         sender.sendMessage(ColorUtils.color(s));
+    }
+    public static void broadcastMessage(CommandSender sender, String s) {
+        Bukkit.broadcastMessage(ColorUtils.color(s));
     }
 }
