d6997bfe82c337f7398149fce17c76eca6e0d27d
Kevin D <kevin.diangelis@gmail.com>
2023-01-11 15:41:32 -0500

    Update ConfigUtils.java
    Signed-off-by: Kevin D <kevin.diangelis@gmail.com>

--- src/me/kevind/utils/ConfigUtils.java
+++ src/me/kevind/utils/ConfigUtils.java
@@ -7,10 +7,10 @@ import java.io.File;
 public final class ConfigUtils {
     public static void createFiles(File File) {
     }
-    public static String getString(String string) {
+    public static String getString(String s) {
         return VibeHub.getInstance().getConfig().getString(s);
     }
-    public static String get(String string) {
+    public static String get(String s) {
         return VibeHub.getInstance().getConfig().get(s);
     }
 }
