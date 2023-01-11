81401a818eafa34b4d0e19d0aa575f4fe5753f09
Kevin D <kevin.diangelis@gmail.com>
2023-01-11 15:17:46 -0500

    add more to configutils

--- src/me/kevind/utils/ConfigUtils.java
+++ src/me/kevind/utils/ConfigUtils.java
@@ -4,10 +4,13 @@ import me.kevind.VibeHub;
 
 import java.io.File;
 
-public class ConfigUtils {
+public final class ConfigUtils {
     public static void createFiles(File File) {
     }
-    public static String getString(String s) {
+    public static String getString(String string) {
         return VibeHub.getInstance().getConfig().getString(s);
+    }
+    public static String get(String string) {
+        return VibeHub.getInstance().getConfig().get(s);
     }
 }
