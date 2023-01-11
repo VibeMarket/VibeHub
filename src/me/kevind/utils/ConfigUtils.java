0b8902560d6af2b4c8a2a575c9937baec36ed825
Kevin D <kevin.diangelis@gmail.com>
2023-01-11 14:11:09 -0500

    slight modifications to ConfigUtils

--- src/me/kevind/utils/ConfigUtils.java
+++ src/me/kevind/utils/ConfigUtils.java
@@ -8,7 +8,6 @@ public class ConfigUtils {
     public static void createFiles(File File) {
     }
     public static String getString(String s) {
-        VibeHub.getInstance().getConfig().getString(s);
-        return s;
+        return VibeHub.getInstance().getConfig().getString(s);
     }
 }
