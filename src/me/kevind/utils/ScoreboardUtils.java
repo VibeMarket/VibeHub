85001ed9d3c7482b3c457044fe9a095a8619fbfd
Kevin D <kevin.diangelis@gmail.com>
2023-03-09 13:20:00 -0500

    changes

--- src/me/kevind/utils/ScoreboardUtils.java
+++ src/me/kevind/utils/ScoreboardUtils.java
@@ -12,6 +12,8 @@ import java.util.Date;
 
 public class ScoreboardUtils {
 
+
+
     public static void setupEntityCollision(Player player) {
             Team team = player.getScoreboard().registerNewTeam("NoPush");
             team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
@@ -19,15 +21,15 @@ public class ScoreboardUtils {
     }
 
     public static void setupScoreboard(Player player) {
-            User user = VibeHub.luckperms.getUserManager().getUser(player.getUniqueId());
-            Date date = new Date(player.getFirstPlayed());
+        User user = VibeHub.luckperms.getUserManager().getUser(player.getUniqueId());
+        Date date = new Date(player.getFirstPlayed());
             SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy");
             String firstplayed = sdf.format(date);
             Objective objective = player.getScoreboard().registerNewObjective("sidebar", "dummy", ColorUtils.color("&9&lVibe &8| &7Hub"), RenderType.INTEGER);
             objective.setDisplaySlot(DisplaySlot.SIDEBAR);
 
             objective.getScore("  ").setScore(6);
-            objective.getScore(ColorUtils.color("&7Name: &f" + PlaceholderAPI.setPlaceholders(player, "%player_name%"))).setScore(5);
+            objective.getScore(ColorUtils.color("&7Name: &f")).setScore(5);
             objective.getScore(ColorUtils.color("&7Rank: &f")).setScore(4);
             objective.getScore(ColorUtils.color("&7Ping &f")).setScore(3);
             objective.getScore(ColorUtils.color("&7Joined: &f" + firstplayed)).setScore(2);
@@ -41,13 +43,25 @@ public class ScoreboardUtils {
             Team ping = player.getScoreboard().registerNewTeam("ping");
             ping.setSuffix(player.getPing() + "ms");
             ping.addEntry(ColorUtils.color("&7Ping &f"));
+
+            Team playerr = player.getScoreboard().registerNewTeam("player");
+            playerr.setSuffix(player.getName());
+            playerr.addEntry(ColorUtils.color("&7Name: &f"));
         }
         public static void updateScoreboard(Player player) {
+            User user = VibeHub.luckperms.getUserManager().getUser(player.getUniqueId());
             Team ping = player.getScoreboard().getTeam("ping");
             if (ping == null) {
                 return;
             }
             ping.setSuffix(player.getPing() + "ms");
             ping.addEntry(ColorUtils.color("&7Ping &f"));
+
+            Team rank = player.getScoreboard().getTeam("rank");
+            if (rank == null) {
+                return;
+            }
+            rank.setSuffix(ColorUtils.color(user.getCachedData().getMetaData().getPrefix()));
+            rank.addEntry(ColorUtils.color("&7Rank: &f"));
         }
     }
