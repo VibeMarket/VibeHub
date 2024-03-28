ea0326980ce337d31dd0649d792d752555df2e13
Kevin D <kevin.diangelis@gmail.com>
2024-03-27 20:03:06 -0400

    Add hex support
    Signed-off-by: Kevin D <kevin.diangelis@gmail.com>

--- src/me/kevind/vibehub/utils/ColorUtils.java
+++ src/me/kevind/vibehub/utils/ColorUtils.java
@@ -2,10 +2,30 @@ package me.kevind.vibehub.utils;
 
 import org.bukkit.ChatColor;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static net.md_5.bungee.api.ChatColor.COLOR_CHAR;
+
 public final class ColorUtils {
 
     public static String color(String string) {
-        return ChatColor.translateAlternateColorCodes('&', string);
+        return ChatColor.translateAlternateColorCodes('&', translateHexColorCodes("&#", "", string));
+    }
+
+    private static String translateHexColorCodes(String startTag, String endTag, String message) {
+        final Pattern hexPattern = Pattern.compile(startTag + "([A-Fa-f0-9]{6})" + endTag);
+        Matcher matcher = hexPattern.matcher(message);
+        StringBuilder buffer = new StringBuilder(message.length() + 4 * 8);
+        while (matcher.find()) {
+            String group = matcher.group(1);
+            matcher.appendReplacement(buffer, COLOR_CHAR + "x"
+                    + COLOR_CHAR + group.charAt(0) + COLOR_CHAR + group.charAt(1)
+                    + COLOR_CHAR + group.charAt(2) + COLOR_CHAR + group.charAt(3)
+                    + COLOR_CHAR + group.charAt(4) + COLOR_CHAR + group.charAt(5)
+            );
+        }
+        return matcher.appendTail(buffer).toString();
     }
 
 }
